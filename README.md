c8e4469df43b61e4b703ad6a54ff08fda6fec89b
LuisGarcia <luis.garcia@tempostorm.com>
2022-10-09 03:09:09 -0500

    Added the smart contract for videos in erc721

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MVC
 My Video Community is a NFT project aiming to allow the people to create their own Video Clip NFTs from a live streaming 
+
+# Video NFT Smart Contract
+
+Smart contract deployed to 0x8ea11069484dA05d463946AFEDa9017503B30afA
+
---
+++ contracts/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "VOk7yOu1rH7bkQ1D1b1aiFKyGq1y7ato";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "0x6b281aeabdf2273532facfede3c4d8871c094cede46b95d082264288c80e2268";
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
+};
